f2ec25e8e3deb2e779c4a49484b6183bbffd7df2
drruin <kdocdoom@gmail.com>
2026-01-18 04:19:24 +0530

    Add comprehensive streaming support: gRPC, multipart, media, SSE heartbeats

--- src/instanton/dashboard/tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Dashboard background tasks."""
-
-from instanton.dashboard.tasks.cleanup import cleanup_old_logs, run_cleanup_loop
-
-__all__ = [
-    "cleanup_old_logs",
-    "run_cleanup_loop",
-]
